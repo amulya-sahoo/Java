fd7e249c35acdce2fb37fb190617a4596c40fc69
Amulya Kumar Sahoo <98418204+amulya-sahoo@users.noreply.github.com>
2022-01-31 18:23:08 +0530

    Create LargeAmongeThree.java

---
+++ LargeAmongeThree.java
@@ -0,0 +1,20 @@
+public class LargeAmongeThree{
+    public static void main(String[] args) {
+        int a = 56;
+        int b = 45;
+        int c = 24;
+        if (a>b){
+            if(a>c)
+                System.out.println("A is greater");
+            else
+                System.out.println("c is greater");}
+        else{
+            if(b>c)
+                System.out.println("b is greater");
+            else
+                System.out.println("c is greater");
+        }
+
+
+    }
+}
